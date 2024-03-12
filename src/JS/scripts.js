0baf01c12a984ddace0e4eee9469c41739fabdc5
vitor rabelo de araujo <vitorrabelodaraujo013@gmail.com>
2024-03-12 17:53:32 -0300

    version01

---
+++ src/JS/scripts.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+function generateQrCode() {
+    console.log('teste');
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode();
+
+});
